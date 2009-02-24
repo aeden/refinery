53f848b1bec4725e271a2b9ef3ee18076fb7fb51
Anthony Eden <anthonyeden@gmail.com>
2009-02-23 23:56:46 -0500

    clean up tests

--- test/unit/daemon_test.rb
+++ test/unit/daemon_test.rb
@@ -3,22 +3,23 @@ class DaemonTest < Test::Unit::TestCase
   context "a daemon" do
     setup do
       @server = stub('Server')
+      @queue = stub('Queue')
     end
     should "be startable" do
       assert_nothing_raised do
-        daemon = Refinery::Daemon.start(@server, 1)
+        daemon = Refinery::Daemon.new(@server, 'sample', @queue)
       end
     end
     should "have logging" do
-      daemon = Refinery::Daemon.start(@server, 1)
+      daemon = Refinery::Daemon.new(@server, 'sample', @queue)
       assert_not_nil daemon.logger
     end
     context "that is started" do
       setup do
-        @daemon = Refinery::Daemon.start(@server, 1)
+        @daemon = Refinery::Daemon.new(@server, 'sample', @queue)
       end
       should "have a state of running" do
-        assert_equal Refinery::Daemon::RUNNING, @daemon.state
+        assert @daemon.running?
       end
     end
   end
