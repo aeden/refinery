1814a26c13bd6a412855d4f3574a2928328733e5
Anthony Eden <anthonyeden@gmail.com>
2009-03-05 23:14:32 -0500

    clean up requires

--- lib/refinery.rb
+++ lib/refinery.rb
@@ -3,27 +3,44 @@ $:.unshift(File.dirname(__FILE__))
 require 'logger'
 require 'benchmark'
 
-require 'rubygems'
-require 'right_aws'
-require 'json'
-
-require 'refinery/loggable'
-require 'refinery/configurable'
-require 'refinery/queueable'
-
-require 'refinery/utilities'
-
-require 'refinery/config'
-require 'refinery/heartbeat'
-require 'refinery/server'
-require 'refinery/daemon'
-require 'refinery/worker'
-require 'refinery/event_publisher'
-require 'refinery/publisher'
-
 # The Refinery module contains all of the classes for the refinery system.
 module Refinery
+  
+  def self.require_library(short_name, display_name)
+    begin
+      require short_name
+    rescue LoadError
+      puts "#{display_name} is required, please install it"
+      exit
+    end
+  end
+  
+  def self.require_libraries
+    require_library('rubygems', 'Rubygems')
+    require_library('right_aws', 'RightScale AWS gem')
+    require_library('json', 'JSON gem')
+  end
+  
+  def self.require_internals
+    require 'refinery/loggable'
+    require 'refinery/configurable'
+    require 'refinery/queueable'
+
+    require 'refinery/utilities'
+
+    require 'refinery/config'
+    require 'refinery/heartbeat'
+    require 'refinery/server'
+    require 'refinery/daemon'
+    require 'refinery/worker'
+    require 'refinery/event_publisher'
+    require 'refinery/publisher'
+  end
+  
   # Raised if a source file cannot be loaded
   class SourceFileNotFound < RuntimeError
   end
 end
+
+Refinery::require_libraries
+Refinery::require_internals
