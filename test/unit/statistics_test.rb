2ee54f9f6bd96b7c95891ad693a75ca2f7ebc918
Anthony Eden <anthonyeden@gmail.com>
2009-03-09 00:48:59 -0400

    basic tests for the statistics class

---
+++ test/unit/statistics_test.rb
@@ -0,0 +1,41 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class StatisticsTest < Test::Unit::TestCase
+  context "a statistics instance" do
+    should "record a done message" do
+      dataset = stub('dataset')
+      dataset.expects(:<<) # TODO: improve this expectation
+      
+      db = stub('db')
+      db.stubs(:table_exists?).returns(true)
+      db.stubs(:[]).with(:completed_jobs).returns(dataset)
+      
+      Sequel.expects(:connect).with('sqlite://stats.db').returns(db)
+      
+      message = {
+        'host_info' => {'hostname' => 'test', 'pid' => 1},
+        'run_time' => 1,
+        'original' => ''
+      }
+      Refinery::Statistics.new.record_done(message)
+    end
+    
+    should "record an error message" do
+      dataset = stub('dataset')
+      dataset.expects(:<<) # TODO: improve this expectation
+      
+      db = stub('db')
+      db.stubs(:table_exists?).returns(true)
+      db.stubs(:[]).with(:errors).returns(dataset)
+      
+      Sequel.expects(:connect).with('sqlite://stats.db').returns(db)
+      
+      message = {
+        'host_info' => {'hostname' => 'test', 'pid' => 1},
+        'error' => {'class' => 'Error', 'message' => 'An error occurred.'},
+        'original' => ''
+      }
+      Refinery::Statistics.new.record_error(message)
+    end
+  end
+end
