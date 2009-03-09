551c6e7b1b7459f83328ee0b7adedb1e0ccb2b43
Anthony Eden <anthonyeden@gmail.com>
2009-03-09 00:51:33 -0400

    more utilities tests

--- test/unit/utilities_test.rb
+++ test/unit/utilities_test.rb
@@ -6,9 +6,20 @@ end
 
 class UtilitiesTest < Test::Unit::TestCase
   context "a class with utilities included" do
+    setup do
+      @o = UtilitiesIncluded.new
+    end
     should "camelize a word" do
-      o = UtilitiesIncluded.new
-      assert_equal 'ClassName', o.camelize('class_name')
+      
+      assert_equal 'ClassName', @o.camelize('class_name')
+    end
+    should "encode and decode message" do
+      message = {'some' => 'message'}
+      assert_equal message, @o.decode_message(@o.encode_message(message))
+    end
+    should "provide host info" do
+      assert_not_nil @o.host_info['pid']
+      assert_not_nil @o.host_info['hostname']
     end
   end
 end
